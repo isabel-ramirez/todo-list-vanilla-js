21605de988bf02db0987ac7c6a8e4ccbe88026cf
isabel-ramirez <80434931+isabel-ramirez@users.noreply.github.com>
2025-09-26 13:33:29 -0500

    feat: Mark todos as completed

--- script.js
+++ script.js
@@ -3,16 +3,27 @@
 // Add new tasks
 const taskInput = document.querySelector('#new-task');
 const activeList = document.querySelector('#active > ul');
+const completedList = document.querySelector('#completed > ul');
 const btn = document.querySelector('#btn-new-task');
 
 btn.addEventListener('click', function () {
   console.log('hello?..');
   const task = document.createElement('li');
   const taskText = document.createElement('span');
+  const checkbox = document.createElement('input');
+
+  checkbox.setAttribute('type', 'checkbox');
 
   taskText.textContent = taskInput.value;
   taskInput.value = '';
 
+  task.appendChild(checkbox);
   task.appendChild(taskText);
   activeList.appendChild(task);
+
+  checkbox.addEventListener('click', function () {
+    completedList.appendChild(task);
+
+    task.firstChild.setAttribute('disabled', true);
+  })
 })
